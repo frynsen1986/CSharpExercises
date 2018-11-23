e1f33e05e8d16f8e90c0bb7afb77107a79165b90
Stefan Hartung Jørgensen <frynsen1986@gmail.com>
2018-11-23 08:54:51 +0100

    Solved Ex_m3_2 - Added structs for Teacher, Student and Grade.

--- Ex_m3_1/Data/Grade.cs
+++ Ex_m3_1/Data/Grade.cs
@@ -8,4 +8,33 @@ namespace GradesPrototype.Data
 {
     // Types of user
     public enum Role { Teacher, Student };
+
+    public struct Grade
+    {
+        int StudentID;
+        string AssessmentDate;
+        string SubjectName;
+        string Assessment;
+        string Comments;
+    }
+
+    public struct Student
+    {
+        int StudentID;
+        string UserName;
+        string Password;
+        int TeacherID;
+        string FirstName;
+        string LastName;
+    }
+
+    public struct Teacher
+    {
+        int TeacherID;
+        string UserName;
+        string Password;
+        string FirstName;
+        string LastName;
+        string Class;
+    }
 }
